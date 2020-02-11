c99822955513c11efc00aa197c8dad89a819a98f
akang-donai <40636630+akang-donai@users.noreply.github.com>
2020-02-11 11:03:59 +0700

    Update P-touch Template
    Update 3 lines of p-touch template command

--- ptseries.sh
+++ ptseries.sh
@@ -11,6 +11,9 @@ sudo apt install lib32z1 -y
 cd Downloads/
 wget https://download.brother.com/welcome/dlfp100274/ptp900lpr-1.0.0-0.i386.deb
 wget https://download.brother.com/welcome/dlfp100276/ptp900cupswrapper-1.0.0-0.i386.deb
+wget https://download.brother.com/welcome/dlfp100293/offfice_template.tar.gz.tar.gz
+mv offfice_template.tar.gz.tar.gz office_template.tar.gz
+gunzip office_template.tar.gz
 mkdir /var/spool/lpd
 sudo dpkg -i --force-all ptp900lpr-1.0.0-0.i386.deb
 sudo dpkg -i --force-all ptp900cupswrapper-1.0.0-0.i386.deb
