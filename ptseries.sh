d0ced8799661e11a4ebc21dd27cc06458c1ff52c
akang-donai <40636630+akang-donai@users.noreply.github.com>
2020-02-11 11:17:56 +0700

    Creating new branch
    added driver download TD-2130N

--- ptseries.sh
+++ ptseries.sh
@@ -9,11 +9,8 @@ sudo apt update && apt upgrade -y
 # Preparing the setup
 sudo apt install lib32z1 -y
 cd Downloads/
-wget https://download.brother.com/welcome/dlfp100274/ptp900lpr-1.0.0-0.i386.deb
-wget https://download.brother.com/welcome/dlfp100276/ptp900cupswrapper-1.0.0-0.i386.deb
-wget https://download.brother.com/welcome/dlfp100293/offfice_template.tar.gz.tar.gz
-mv offfice_template.tar.gz.tar.gz office_template.tar.gz
-gunzip office_template.tar.gz
+wget https://download.brother.com/welcome/dlfp002222/td2130nlpr-1.0.1-0.i386.deb
+wget https://download.brother.com/welcome/dlfp100008/td2130ncupswrapper-1.0.1-0.i386.deb
 mkdir /var/spool/lpd
-sudo dpkg -i --force-all ptp900lpr-1.0.0-0.i386.deb
-sudo dpkg -i --force-all ptp900cupswrapper-1.0.0-0.i386.deb
+sudo dpkg -i --force-all td2130nlpr-1.0.1-0.i386.deb
+sudo dpkg -i --force-all td2130ncupswrapper-1.0.1-0.i386.deb
